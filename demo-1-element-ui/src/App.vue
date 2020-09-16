fae3fd3bfcc831f8ad5a835a415a73d0f59caf0f
Dec <2312225342@qq.com>
2020-09-16 23:47:20 +0800

    add Login.vue add normalize.css

--- demo-1-element-ui/src/App.vue
+++ demo-1-element-ui/src/App.vue
@@ -1,20 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <div>
-      <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
-      </p>
-      <el-button>el-button</el-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <router-view></router-view>
+
   </div>
+
+
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'app',
@@ -25,12 +21,13 @@ export default {
 </script>
 
 <style>
+@import "assets/css/normalize.css";
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
---
+++ demo-1-element-ui/src/views/login/Login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="login">
+<!--    <div class="statement">-->
+<!--      早安心语：最美好的是相遇，最难得的是重逢-->
+<!--    </div>-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login"
+}
+</script>
+
+<style scoped>
+#login {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  background: linear-gradient(134deg, #F66BA9, #A8EEF1);
+  /*background-size:100% 100%;*/
+
+  width: 100%;
+  height: 100vh;
+}
+
+.statement {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  line-height: 100px;
+  text-align: center;
+  font-size: 50px;
+  /*background: linear-gradient(93deg, #82C815, #F5D437);*/
+  /*background-color: #42b983;*/
+  width: 100%;
+}
+
+</style>
